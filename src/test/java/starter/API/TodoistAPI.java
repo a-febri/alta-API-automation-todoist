f4b4ae384e3e80824dd79ab4798572fd8c21d45b
Ari Febri Anto <arifebri12@gmail.com>
2022-10-05 18:38:28 +0700

    create feature branch

---
+++ src/test/java/starter/API/TodoistAPI.java
@@ -0,0 +1,10 @@
+package starter.API;
+
+public class TodoistAPI {
+    public static String URL = "https://api.todoist.com";
+
+
+    public static final String DIR = System.getProperty("user.dir");
+    public static final String JSON_REQUEST_BODY_FILE = DIR+"/src/test/resources/JSON/RequestBody";
+    public static final String JSON_SCHEMA_VALIDATOR_FILE = DIR+"/src/test/resources/JSON/JsonSchema";
+}
