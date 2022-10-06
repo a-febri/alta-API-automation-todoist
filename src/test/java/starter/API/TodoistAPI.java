ee92a0601d34a1859a4d03440949ae0c747c4b80
yusrilfauzan77@gmail.com <yusrilfauzan77@gmail.com>
2022-10-06 22:26:23 +0700

    get project and post create new project

--- src/test/java/starter/API/TodoistAPI.java
+++ src/test/java/starter/API/TodoistAPI.java
@@ -1,9 +1,46 @@
 package starter.API;
 
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import java.io.File;
+
 public class TodoistAPI {
     public static String URL = "https://api.todoist.com";
     public static String AUTH = "d256eb8544ab45ff968db31541e1f058ce0ce5d6";
+    public static String GET_PROJECT = URL+"/rest/v1/projects/{id}";
+    public static String GET_PROJECT_INVALID = URL+"/rest/v1/projects=/=/";
+    public static String POST_CREATE_PROJECT = URL+"/rest/v1/projects";
+    public static String POST_CREATE_PROJECT_INVALID = URL+"/rest/v1=/=/=/projects";
     public static final String DIR = System.getProperty("user.dir");
     public static final String JSON_REQUEST_BODY_FILE = DIR+"/src/test/resources/JSON/RequestBody";
     public static final String JSON_SCHEMA_VALIDATOR_FILE = DIR+"/src/test/resources/JSON/JsonSchema";
+
+    @Step("Post Create Project Authorization")
+    public void setPostCreateProject(File json){
+        SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .headers("Authorization", "Bearer d256eb8544ab45ff968db31541e1f058ce0ce5d6");
+    }
+
+    @Step("Post Create Project Unauthorized")
+    public void setPostCreateProjectUnauthorized(File json){
+        SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+    @Step("Get projects")
+    public void getProjects(String id){
+        SerenityRest.given()
+                .headers("Authorization", "Bearer d256eb8544ab45ff968db31541e1f058ce0ce5d6")
+                .pathParam("id", id);
+    }
+
+    @Step("Get projects without auth")
+    public void getProjectsWithoutAuth(String id){
+        SerenityRest.given()
+                .pathParam("id", id);
+    }
 }
--- src/test/java/starter/ResponseBody/TodoistResponse.java
+++ src/test/java/starter/ResponseBody/TodoistResponse.java
@@ -2,4 +2,7 @@ package starter.ResponseBody;
 
 public class TodoistResponse {
 
+    public static String ID = "id";
+    public static String NAME = "name";
+    public static String COLOR = "color";
 }
